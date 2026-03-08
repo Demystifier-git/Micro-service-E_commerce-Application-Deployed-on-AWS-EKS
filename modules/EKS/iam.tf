1c9cda98b9323cc1390143805bf505199e7ed964
devops4sure <devops4sure@gmail.com>
2026-03-08 06:13:57 -0700

    Initial commit

--- modules/EKS/iam.tf
+++ modules/EKS/iam.tf
@@ -63,3 +63,31 @@ resource "aws_iam_role_policy_attachment" "node_attach" {
   policy_arn = aws_iam_policy.node_app_access.arn
 
 }
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "${var.cluster_name}-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_readonly_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
--- modules/EKS/node-groups.tf
+++ modules/EKS/node-groups.tf
@@ -1,29 +1,41 @@
+# IAM role for worker nodes
 resource "aws_iam_role" "node_role" {
-
   name = "${var.cluster_name}-node-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-
     Statement = [{
-      Effect = "Allow"
+      Effect    = "Allow"
       Principal = { Service = "ec2.amazonaws.com" }
-      Action = "sts:AssumeRole"
+      Action    = "sts:AssumeRole"
     }]
   })
-
 }
 
-resource "aws_eks_node_group" "nodes" {
+# Attach required policies
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
 
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_readonly_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# Node groups
+resource "aws_eks_node_group" "nodes" {
   for_each = var.node_groups
 
   cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = each.key
-
-  node_role_arn = aws_iam_role.node_role.arn
-
-  subnet_ids = var.private_subnets
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = var.private_subnets
 
   instance_types = each.value.instance_types
   capacity_type  = each.value.capacity_type
@@ -33,5 +45,32 @@ resource "aws_eks_node_group" "nodes" {
     min_size     = each.value.min_size
     max_size     = each.value.max_size
   }
+}
 
+# aws-auth ConfigMap so nodes can join the cluster
+provider "kubernetes" {
+  host                   = aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.cluster.name
+}
+
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = yamlencode([
+      {
+        rolearn  = aws_iam_role.node_role.arn
+        username = "system:node:{{EC2PrivateDNSName}}"
+        groups   = ["system:bootstrappers", "system:nodes"]
+      }
+    ])
+  }
 }
