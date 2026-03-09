4402206c82427e85adb2feab2b19b3f1ad41b42e
devops4sure <devops4sure@gmail.com>
2026-03-09 07:20:30 -0700

    new file

--- modules/EKS/node-groups.tf
+++ modules/EKS/node-groups.tf
@@ -1,16 +1,12 @@
 ############################################
 # EKS Node Bootstrap Script
 ############################################
-data "template_file" "bootstrap" {
-  template = <<-EOF
+locals {
+  bootstrap_script = <<-EOF
     #!/bin/bash
     set -o xtrace
-    /etc/eks/bootstrap.sh ${var.cluster_name}
+    /etc/eks/bootstrap.sh ${aws_eks_cluster.cluster.name}
   EOF
-
-  vars = {
-    cluster_name = aws_eks_cluster.cluster.name
-  }
 }
 
 ############################################
@@ -19,13 +15,10 @@ data "template_file" "bootstrap" {
 resource "aws_launch_template" "eks_nodes_lt" {
   name_prefix = "eks-nodes-"
 
-  # Assign the custom security group
   vpc_security_group_ids = [aws_security_group.eks_nodes.id]
 
-  # Bootstrap user data for EKS
-  user_data = base64encode(data.template_file.bootstrap.rendered)
+  user_data = base64encode(local.bootstrap_script)
 
-  # Tags for the EC2 instances
   tag_specifications {
     resource_type = "instance"
     tags = {
@@ -42,30 +35,23 @@ resource "aws_eks_node_group" "nodes" {
 
   cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = each.key
+  node_role_arn   = aws_iam_role.eks_node_role.arn
 
-  node_role_arn = aws_iam_role.eks_node_role.arn
+  subnet_ids       = var.private_subnets
+  instance_types   = each.value.instance_types
+  capacity_type    = each.value.capacity_type
 
-  # Nodes will launch in these subnets
-  subnet_ids = var.private_subnets
-
-  # Instance type & capacity
-  instance_types = each.value.instance_types
-  capacity_type  = each.value.capacity_type
-
-  # Auto-scaling configuration
   scaling_config {
     desired_size = each.value.desired_size
     min_size     = each.value.min_size
     max_size     = each.value.max_size
   }
 
-  # Attach the launch template
   launch_template {
     id      = aws_launch_template.eks_nodes_lt.id
-    version = "$Latest"  # Always use the latest version of the LT
+    version = "$Latest"  # Terraform accepts $Latest in quotes
   }
 
-  # Ensure IAM policies are attached before provisioning nodes
   depends_on = [
     aws_iam_role_policy_attachment.eks_worker_node_policy,
     aws_iam_role_policy_attachment.eks_cni_policy,
